3ecfcdd17d01ff01878e0200ddd92a3426568af6
eshita201 <eshita201@gmail.com>
2022-10-03 15:11:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const  path = require( 'path');
+const dotenv=require('dotenv');
 const app = express();
-const PORT=3000;
+dotenv.config({path:'config.env'})
+const PORT=process.env.PORT
 app.use( express.static(__dirname + '/dist/portfolio'));
 app.listen(PORT,()=>{console.log(`running on http://localhost:${PORT}`)});
